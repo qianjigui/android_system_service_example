37fe0565b33f3ed2b34074342abb6dab4e9b7c65
qianjigui <qianjigui@gmail.com>
2014-02-26 16:39:14 +0800

    Remove unused codes @ java server

--- javaserver/src/com/example/demoapiservice/MainActivity.java
+++ javaserver/src/com/example/demoapiservice/MainActivity.java
@@ -14,53 +14,31 @@ import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-    
-    IDemoAPI binder;
-    
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            binder = (IDemoAPI) service;
-        }
-        
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            binder = null;
-        }
-        
-    };
-    
-    static final String SERVICE_DESC_STRING = "com.example.demoapiservice.DemoAPIService";
-    
+     static final String SERVICE_NAME="android.binder.example";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Object object = new Object();
-        Method addService;
-        try {
-            addService = Class.forName("android.os.ServiceManager").getMethod("addService", String.class, IBinder.class);
-            addService.invoke(object, new Object[]{DemoAPIService.SERVICE_NAME, new BnDemoAPI()});
-        } catch (Exception e) {
-            e.printStackTrace();
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Object object = new Object();
+            Method addService;
+            try {
+                addService = Class.forName("android.os.ServiceManager").getMethod("addService", String.class, IBinder.class);
+                addService.invoke(object, new Object[]{SERVICE_NAME, new BnDemoAPI()});
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        //this.startService(new Intent(SERVICE_DESC_STRING));
-        // this.bindService(
-        // new Intent(SERVICE_DESC_STRING), serviceConnection,
-        // BIND_AUTO_CREATE);
-    }
-    
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+
     @Override
-    protected void onDestroy() {
-        //this.unbindService(serviceConnection);
-        super.onDestroy();
-    }
-    
+        protected void onDestroy() {
+            //this.unbindService(serviceConnection);
+            super.onDestroy();
+        }
+
 }
